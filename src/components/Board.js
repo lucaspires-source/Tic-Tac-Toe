116516d2c44fb21406d6f1be0bae1a96b5bbb198
@lucaspires-source <pires.lucas94@gmail.com>
2021-03-27 14:49:49 -0300

    Styling square component

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import Square from './Square';
-
+const style ={
+    background:'lightblue',
+    border: '2px solid darkblue',
+    fontSize: '30px',
+    fontWeight:'800',
+    cursor:'pointer',
+    outline: 'none'
+}
 
 
 const Board = ({squares, onClick}) => {
